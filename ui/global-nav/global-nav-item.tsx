5689d703b3db25bd5d9b3564fe29ae844f09e3f8
Hosein Hoseiny <seied78@gmail.com>
2023-09-27 08:24:38 +0330

    🏗️ refactor: breaking project to smaller components
    breaking global nav component to new smaller global nav component and global nav item component

---
+++ ui/global-nav/global-nav-item.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { type Item } from '#/lib/items';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
+
+export function GlobalNavItem({
+  item,
+  close,
+}: {
+  item: Item;
+  close: () => false | void;
+}) {
+  // can be usefull to detect the # tag
+  const segment = useSelectedLayoutSegment();
+
+  // todo add logic to check if is active
+  const isActive = true;
+
+  return (
+    <Link
+      onClick={close}
+      href={`/`}
+      // href={`/${item._id_can_be_added_to_object}`}
+      className={clsx(
+        'block rounded-md px-3 py-2 text-sm font-medium hover:text-gray-300',
+        {
+          'text-gray-400 hover:bg-gray-800': !isActive,
+          'text-white': isActive,
+        },
+      )}
+    >
+      {item.name}
+    </Link>
+  );
+}
