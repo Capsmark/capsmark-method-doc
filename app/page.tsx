87f0d187bae43832a5d96fd01a4f5026f3c5526b
Amir <farhaadi.amir@gmail.com>
2023-09-28 03:04:46 +0330

    🐛 Fix: added key when mapping sections

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
   return (
     <div className="space-y-8">
       <div className="space-y-10 text-white">
-        {items.map(({ name, items, id }, index) => (
-          <Section name={name} items={items} id={id} index={index} />
+        {items.map(({ id, name, items }, index) => (
+          <Section key={id} id={id} name={name} items={items} index={index} />
         ))}
       </div>
     </div>
