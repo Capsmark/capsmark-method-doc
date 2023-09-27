b5e8bd862a77197927f66b076bbebb0633b20015
Amir <farhaadi.amir@gmail.com>
2023-09-27 18:40:04 +0330

    🚚 Fix: changed pathing and image config

--- lib/items.ts
+++ lib/items.ts
@@ -1,18 +1,12 @@
-import chart1 from '../public/charts/chart1.png'
-import chart2 from '../public/charts/chart2.png'
-import chart3 from '../public/charts/chart3.png'
-import chart4 from '../public/charts/chart4.png'
-import chart5 from '../public/charts/chart5.png'
-import chart6 from '../public/charts/chart6.png'
-import chart7 from '../public/charts/chart7.png'
-import chart8 from '../public/charts/chart8.png'
-import chart9 from '../public/charts/chart9.png'
-import chart10 from '../public/charts/chart10.png'
-import chart11 from '../public/charts/chart11.png'
-import chart12 from '../public/charts/chart12.png'
-import chart13 from '../public/charts/chart13.png'
-import chart14 from '../public/charts/chart14.png'
-import { StaticImageData } from '#/node_modules/next/image'
+import { StaticImageData } from '#/node_modules/next/image';
+import chart1 from '/public/charts/chart1.png';
+import chart2 from '/public/charts/chart2.png';
+import chart3 from '/public/charts/chart3.png';
+import chart4 from '/public/charts/chart4.png';
+import chart5 from '/public/charts/chart5.png';
+import chart6 from '/public/charts/chart6.png';
+import chart7 from '/public/charts/chart7.png';
+import chart9 from '/public/charts/chart9.png';
 
 export type Item = {
   name: string;
@@ -29,14 +23,14 @@ type Picture = {
   firstSetOfList?: List[];
   secondSetOfParagraphs?: string[];
   secondSetOfList?: List[];
-}
+};
 
 type List = {
   ul?: boolean;
   listItems: string[];
-}
+};
 
-export const items: { name: string; items: Item[], id: string }[] = [
+export const items: { name: string; items: Item[]; id: string }[] = [
   {
     name: 'CAPSMARK Story, Models & Future',
     id: 'story',
@@ -54,7 +48,7 @@ export const items: { name: string; items: Item[], id: string }[] = [
           `While our modus operandi echoes many peers in the hedge fund and asset management sphere, a defining hallmark sets us apart: our trust in artificial intelligence to helm tasks that traditionally challenge human capacities.`,
           `In our commitment to thorough validation, we compared our methodology to the benchmark of the S&P500, carefully removing features specific to Cryptocurrencies. Leveraging our expertise, we successfully refined our methodology to other key financial sectors, notably the stock market, using a decade's worth of data on the S&P 500. We've detailed these findings in the 'S&P 500 Multi-Transformers (Beta)' appendix. `,
           `These results mainly showcase how adaptable our methodology is. To achieve a confidence level similar to our models in the cryptocurrency realm, a dedicated team would need to engage in deep research for at least six months to produce a reliable version.`,
-        ]
+        ],
       },
     ],
   },
@@ -66,54 +60,82 @@ export const items: { name: string; items: Item[], id: string }[] = [
         name: 'overview',
         id: 'signature-overview',
         range: null,
-        paragraphs: [`Let's look closer at our Signature Fund to understand its workings. Developing a complete robo-advisory trading system requires a solid grasp of the market. This means assessing risks, analyzing the behavior of major Bitcoin players, understanding crypto exchanges, and keeping track of key global economic factors and events.`],
-        picture: [{
-          url: chart9,
-          text: `An overview of the datasets that the model uses for analysis at the time of completion.`,
-        },]
+        paragraphs: [
+          `Let's look closer at our Signature Fund to understand its workings. Developing a complete robo-advisory trading system requires a solid grasp of the market. This means assessing risks, analyzing the behavior of major Bitcoin players, understanding crypto exchanges, and keeping track of key global economic factors and events.`,
+        ],
+        picture: [
+          {
+            url: chart9,
+            text: `An overview of the datasets that the model uses for analysis at the time of completion.`,
+          },
+        ],
       },
       {
         name: 'Step 1.',
         id: 'signature-step-1',
         range: null,
-        paragraphs: [`Bot1 measures the daily relative weight of Bitcoin holders (Exchanges, Miners, Whales) to Bitcoin’s total supply using a simple linear algorithm to calculate the selling power of each group and then passes it to Bot2.`, `Bot2 uses Bot1’s output as input alongside four other metrics (Historical Bitcoin price changes after each halving, historical price volatility, historical network’s hashrate and difficulty) to the Long Short Term Memory Neural Networks engine, which is responsible for capturing long-term dependencies between Bitcoin price data, it's fundamental events and estimating the level of reserved risk/reward based on Bitcoin’s historical cycles with supply-adjusted weights.`],
+        paragraphs: [
+          `Bot1 measures the daily relative weight of Bitcoin holders (Exchanges, Miners, Whales) to Bitcoin’s total supply using a simple linear algorithm to calculate the selling power of each group and then passes it to Bot2.`,
+          `Bot2 uses Bot1’s output as input alongside four other metrics (Historical Bitcoin price changes after each halving, historical price volatility, historical network’s hashrate and difficulty) to the Long Short Term Memory Neural Networks engine, which is responsible for capturing long-term dependencies between Bitcoin price data, it's fundamental events and estimating the level of reserved risk/reward based on Bitcoin’s historical cycles with supply-adjusted weights.`,
+        ],
         picture: [
           {
             url: chart1,
-            firstSetOfParagraphs: [`For instance, this is how Bot2 sees the last 2,000 days’ reserved risk of the market based on Bitcoin’s previous cycles:`],
+            firstSetOfParagraphs: [
+              `For instance, this is how Bot2 sees the last 2,000 days’ reserved risk of the market based on Bitcoin’s previous cycles:`,
+            ],
           },
           {
             url: chart7,
             text: `The higher the values, the lower the reserved risk level is.`,
-            firstSetOfParagraphs: [`Inside Bot3, which works in parallel with Bot1 and Bot2, are two AI engines in series. The first one benefits from a Gaussian Mixture Model, Graph Neural Networks and other techniques to cluster the Bitcoin holder’s wallets into our 36 desired clusters based on each wallet’s historical data and transactions using billions of data points. Then, our Ensemble AI Engine will do the rest to measure the real-time risk magnitude each cluster is bringing to the market.`],
+            firstSetOfParagraphs: [
+              `Inside Bot3, which works in parallel with Bot1 and Bot2, are two AI engines in series. The first one benefits from a Gaussian Mixture Model, Graph Neural Networks and other techniques to cluster the Bitcoin holder’s wallets into our 36 desired clusters based on each wallet’s historical data and transactions using billions of data points. Then, our Ensemble AI Engine will do the rest to measure the real-time risk magnitude each cluster is bringing to the market.`,
+            ],
           },
           {
             url: chart2,
-            firstSetOfParagraphs: [`Bot4 operates in parallel with Bots 1, 2, and 3. However, all computation and clustering techniques focus on the Miners' Wallets data during this process. The objective is to measure the real-time risk magnitude for each of the six identified clusters within the mining sector and determine their impact on the market.`]
+            firstSetOfParagraphs: [
+              `Bot4 operates in parallel with Bots 1, 2, and 3. However, all computation and clustering techniques focus on the Miners' Wallets data during this process. The objective is to measure the real-time risk magnitude for each of the six identified clusters within the mining sector and determine their impact on the market.`,
+            ],
           },
           {
             url: chart3,
-            firstSetOfParagraphs: [`Similarly, Bot5 works in tandem with other bots, concentrating on evaluating the risk magnitude arising from exchanges. This emphasis is crucial, as crypto exchanges hold significant sway over price fluctuations.`]
+            firstSetOfParagraphs: [
+              `Similarly, Bot5 works in tandem with other bots, concentrating on evaluating the risk magnitude arising from exchanges. This emphasis is crucial, as crypto exchanges hold significant sway over price fluctuations.`,
+            ],
           },
           {
             url: chart4,
-            firstSetOfParagraphs: [`With the reserved risk from each sector now accurately gauged, our advanced Hybrid RNN Engine steps in. It aggregates these risks, factoring in the weight assigned to each industry. This weight determination comes from thoroughly examining the sector's historical data and performance. Specifically, the engine carries out three critical tasks:`],
+            firstSetOfParagraphs: [
+              `With the reserved risk from each sector now accurately gauged, our advanced Hybrid RNN Engine steps in. It aggregates these risks, factoring in the weight assigned to each industry. This weight determination comes from thoroughly examining the sector's historical data and performance. Specifically, the engine carries out three critical tasks:`,
+            ],
             firstSetOfList: [
               {
-                listItems: [`Predicting the magnitude of the forthcoming Pump/Dump,`, `Estimating the duration of the next Pump/Dump, and`, `Suggesting suitable position sizing based on the aggregated risk level of the current market situation.`],
-              }
+                listItems: [
+                  `Predicting the magnitude of the forthcoming Pump/Dump,`,
+                  `Estimating the duration of the next Pump/Dump, and`,
+                  `Suggesting suitable position sizing based on the aggregated risk level of the current market situation.`,
+                ],
+              },
             ],
-            secondSetOfParagraphs: [`The trio of recommendations, each paired with its occurrence probability, is relayed to a Trader or Asset Manager. This detailed insight equips the Asset Manager to effectively gauge and scrutinize the imminent reserved risk, especially those stemming from On-Chain activities and crypto exchanges, ensuring enhanced risk mitigation strategies are in place."`]
+            secondSetOfParagraphs: [
+              `The trio of recommendations, each paired with its occurrence probability, is relayed to a Trader or Asset Manager. This detailed insight equips the Asset Manager to effectively gauge and scrutinize the imminent reserved risk, especially those stemming from On-Chain activities and crypto exchanges, ensuring enhanced risk mitigation strategies are in place."`,
+            ],
           },
           {
             url: chart5,
-            firstSetOfParagraphs: [`To maintain and continuously improve robustness and precision, we've seamlessly integrated a Reinforcement Learning Agent. This agent bridges the gap between the RNN Engine's outputs and the initial weights assigned to each sector (W1, W2, W3, W4). Its primary role is to monitor discrepancies between the RNN Engine's forecasts and real-world results, enabling real-time fine-tuning of the RNN Engine and timely adjustments to the weights.`]
+            firstSetOfParagraphs: [
+              `To maintain and continuously improve robustness and precision, we've seamlessly integrated a Reinforcement Learning Agent. This agent bridges the gap between the RNN Engine's outputs and the initial weights assigned to each sector (W1, W2, W3, W4). Its primary role is to monitor discrepancies between the RNN Engine's forecasts and real-world results, enabling real-time fine-tuning of the RNN Engine and timely adjustments to the weights.`,
+            ],
           },
           {
             url: chart6,
-            firstSetOfParagraphs: [`Now, let’s see how the model can benefit a trader or any trading algorithm to help them measure the market’s reserved risk simultaneously, improving their performance and helping them make more accurate decisions.`, `As an illustration, we will opt for a straightforward trading strategy employing the Moving Average Convergence Divergence (MACD) indicator to exclusively generate sell signals. This strategy will be assessed over a five-year span, encompassing historical daily price data for Bitcoin from March 2018 to September 2023. Our aim is to enhance its efficacy through the utilization of our model as an observer of the signals generated by the strategy. We commence with an initial capital of $1,000 and implement a policy that restricts the strategy from allocating more than 50% of its capital to any single trade.`]
-          }
-        ]
+            firstSetOfParagraphs: [
+              `Now, let’s see how the model can benefit a trader or any trading algorithm to help them measure the market’s reserved risk simultaneously, improving their performance and helping them make more accurate decisions.`,
+              `As an illustration, we will opt for a straightforward trading strategy employing the Moving Average Convergence Divergence (MACD) indicator to exclusively generate sell signals. This strategy will be assessed over a five-year span, encompassing historical daily price data for Bitcoin from March 2018 to September 2023. Our aim is to enhance its efficacy through the utilization of our model as an observer of the signals generated by the strategy. We commence with an initial capital of $1,000 and implement a policy that restricts the strategy from allocating more than 50% of its capital to any single trade.`,
+            ],
+          },
+        ],
       },
       {
         name: 'MACD strategy inputs:',
@@ -123,9 +145,8 @@ export const items: { name: string; items: Item[], id: string }[] = [
         picture: [
           {
             url: chart7,
-
-          }
-        ]
+          },
+        ],
       },
     ],
   },
