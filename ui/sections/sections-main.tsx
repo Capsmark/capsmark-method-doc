41dffeb4a70cd1c90484a0cf81b2d36d27d5335a
Hosein Hoseiny <seied78@gmail.com>
2023-09-28 07:44:04 +0330

    :bug: fix: ordered and unordered list fixed

--- ui/sections/sections-main.tsx
+++ ui/sections/sections-main.tsx
@@ -18,13 +18,26 @@ export function SectionsMain({ item }: { item: Item }) {
                 pic.firstSetOfParagraphs.map((par) => (
                   <p className="mb-6 mt-6">{par}</p>
                 ))}
-              {pic.firstSetOfList && (
-                <ol>
-                  {pic.firstSetOfList.map((listItem) => (
-                    <li>{listItem.listItems}</li>
-                  ))}
-                </ol>
-              )}
+              {pic.firstSetOfList &&
+                pic.firstSetOfList.map((listItem) => (
+                  <ol className="ml-8">
+                    {listItem.listItems.map((l, index) => (
+                      <li>
+                        {listItem.ul ? (
+                          <div className="align-center flex">
+                            <div className="mr-2 mt-1 h-2 w-2 rounded-full bg-black"></div>{' '}
+                            {l}
+                          </div>
+                        ) : (
+                          <div>
+                            {index + 1}. {l}
+                          </div>
+                        )}
+                      </li>
+                    ))}
+                  </ol>
+                ))}
+
               {pic.secondSetOfParagraphs &&
                 pic.secondSetOfParagraphs.map((par) => (
                   <p className="mb-6 mt-6">{par}</p>
